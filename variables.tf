a46577b418c2c2765001ce7dcc714e4aa46991ff
Pratibha251093 <158164423+Pratibha251093@users.noreply.github.com>
2024-09-06 16:52:48 +0530

    Update variables.tf
    changes

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "abc" {
-  default = "AKIAXZEFIAYJY35Z27PQ"
+  default = "accesskey"
 }
 
 variable "secabc" {
-    default = "kRxOlSNyNzyt1uhNuZ5Bo1uB/VpRUVGKhB+c6kqu"
+    default = "secrete accesskey"
 }
 
 variable "region" {
