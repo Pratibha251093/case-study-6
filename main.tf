51010e59297cb1b32551767fe4de5b6cfdda4c30
Pratibha251093 <158164423+Pratibha251093@users.noreply.github.com>
2024-09-14 17:00:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,46 +16,6 @@ resource "aws_vpc" "my_vpc" {
   }
 }
 
-# Internet Gateway
-resource "aws_internet_gateway" "my_ig" {
-  vpc_id = aws_vpc.my_vpc.id
-
-  tags = {
-    Name = "my-internet-gateway"
-  }
-}
-
-# Public Subnet
-resource "aws_subnet" "my_subnet" {
-  vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-2a"
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "my-subnet"
-  }
-}
-
-# Route Table
-resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.my_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.my_ig.id
-  }
-
-  tags = {
-    Name = "my-public-route-table"
-  }
-}
-
-# Route Table Association
-resource "aws_route_table_association" "public_route_table_association" {
-  subnet_id      = aws_subnet.my_subnet.id
-  route_table_id = aws_route_table.public_route_table.id
-}
-
 # S3 Bucket for CloudTrail logs
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "my-unique-cloudtrail-logs-bucket-casestudy-6"
